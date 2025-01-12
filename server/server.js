4ce70042353b3283326cbd4645bae64b735e1679
wazirul8261 <hutasona@gmail.com>
2025-01-13 00:32:50 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrgins = ['http://localhost:5173']
+const allowedOrgins = ['https://wazir7-frontend.onrender.com/']
 
 app.use(express.json());
 app.use(cookieParser());
