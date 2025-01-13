4bf90bf4e391b780034e89a51d18f167e7d862a0
wazirul8261 <hutasona@gmail.com>
2025-01-13 10:13:07 +0600

    Update AppContent.jsx

--- p11/src/content/AppContent.jsx
+++ p11/src/content/AppContent.jsx
@@ -8,7 +8,7 @@ export const AppContextProvider = (props)=>{
 
     axios .defaults.withCredentials = true;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://wazir7-backend.onrender.com"
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
 
